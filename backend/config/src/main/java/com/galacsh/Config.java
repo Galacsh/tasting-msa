2ad47d5d218315d9dd9dccf77328387bef0f3228
Galacsh <galacsh.dev@gmail.com>
2025-01-13 15:59:56 +0900

    refactor: rename classes

--- backend/config/src/main/java/com/galacsh/Config.java
+++ backend/config/src/main/java/com/galacsh/Config.java
@@ -2,10 +2,12 @@ package com.galacsh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class AuthServer {
+@EnableConfigServer
+public class Config {
     public static void main(String[] args) {
-        SpringApplication.run(AuthServer.class, args);
+        SpringApplication.run(Config.class, args);
     }
 }
