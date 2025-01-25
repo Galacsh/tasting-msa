4fd5a1c7cfe158b7af3e165f5d704656e58b7f6a
Galacsh <galacsh.dev@gmail.com>
2025-01-25 13:28:39 +0900

    feat: exchange session to passport

--- backend/auth/src/main/java/com/galacsh/controller/PassportController.java
+++ backend/auth/src/main/java/com/galacsh/controller/PassportController.java
@@ -2,14 +2,15 @@ package com.galacsh.controller;
 
 import com.galacsh.Passport;
 import com.galacsh.Response;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PassportController {
     @GetMapping("/passports")
-    public Response<Passport> exchangeToPassport(@RequestParam("vvrite_id") String sessionId) {
-        throw new RuntimeException("Not implemented yet");
+    public Response<Passport> exchangeToPassport(Authentication authentication) {
+        var passport = new Passport(authentication.getName());
+        return Response.ok(passport);
     }
 }
