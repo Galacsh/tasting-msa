631504e953e055aa4134e3f6e27d81f40c8babea
Galacsh <galacsh.dev@gmail.com>
2025-01-25 13:12:07 +0900

    chore: add OAuth2 client dependency

--- backend/auth/build.gradle.kts
+++ backend/auth/build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.retry:spring-retry")
@@ -47,7 +48,7 @@ tasks.jar {
     enabled = false
 }
 
-springBoot{
+springBoot {
     buildInfo {
         properties {
             name = project.name
